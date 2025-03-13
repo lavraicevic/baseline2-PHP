405151253021709e5bcbca95b30cbcbfb246c0ed
lavraicevic <raiceviclav2107@gmail.com>
2025-03-13 19:15:06 +0100

    Task 3 finished
    Data Input and Validation: Validate the form inputs to ensure that the task name is not empty and the due date is a valid date in the future. Display appropriate error messages if validation fails.

--- index.php
+++ index.php
@@ -1,6 +1,23 @@
 <?php
-date_default_timezone_set('Europe/Belgrade')
-
+date_default_timezone_set('Europe/Belgrade');
+$name_error = '';
+$date_error = '';
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    $title = $_POST["title"];
+    if(strlen($title) < 3){
+        $name_error = 'Not enough letters';
+    }else{
+        $name_error = '';
+    }
+    $date = $_POST["due_date"];
+    $time = strtotime($date);
+    if($time < time()){
+        $date_error = 'Please use dates in the future';
+    }else{
+        $date_error = '';
+    }
+    
+}
 ?>
 
 <!DOCTYPE html>
@@ -10,6 +27,7 @@ date_default_timezone_set('Europe/Belgrade')
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Baseline check 2</title>
     <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+
 </head>
 <body class="h-full bg-black">
 
@@ -26,25 +44,36 @@ date_default_timezone_set('Europe/Belgrade')
 
         <form method="POST" class="flex flex-col gap-2 w-[320px]">
 
-            <label class="text-slate-200">Title: 
+            <label class="text-slate-200">Name : 
                 <input class="border-2 border-slate-200 rounded-lg pl-1" type="text" name="title" id="">
+                <p class="text-red-600"><?= $name_error ?></p>
             </label>
 
             <label class="text-slate-200">Description: <br>
-                <textarea class="border-2 border-slate-200 rounded-lg" name="title" rows="4" cols="40"></textarea>
+                <textarea class="border-2 border-slate-200 rounded-lg w-full" name="description" rows="4" cols="40"></textarea>
             </label>
 
 
             <label class="text-slate-200">Due date: 
                 <input class="border-2 border-slate-200 rounded-lg pl-1" type="date" name="due_date" id="">
+                <p class="text-red-600"><?= $date_error ?></p>
             </label>
 
             <input class=" mt-2 border-2 border-slate-200 rounded-lg flex justify-center items-center text-zinc-200 w-30 transition-linear duration-300 hover:bg-zinc-200 hover:text-black " type="submit" value="Add to do">
         </form>
 
+            <!-- Output -->
+
+        <ul class="w-[320px]">
+            <li class="text-zinc-200">
+                <h2 class="font-semibold text-xl mb-1">Take the dog out</h2>
+                <p class="text-neutral-200 text-sm">Go with Dona throught the park, take her to meet the other dogs and to mark teretories. Be carefull about the other more aggresive dogs</p>
+                <button class="mt-3 px-4 py-1 border-2 border-red-600 text-red-600 transition-linear duration-300 hover:bg-red-600 hover:text-black hover:border-red-600">Delete</button>
+            </li>
+        </ul>
+
     </div>
 
-    <!-- Output -->
 
     
     
